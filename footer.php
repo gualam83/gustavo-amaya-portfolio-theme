e601492011c87fa7e59bcad693be1383f195d906
Gustavo Amaya B <gualam@gmail.com>
2026-02-10 11:25:04 -0500

    Initial commit - Gustavo Amaya portfolio theme

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package gustavo-amaya
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gustavo-amaya' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'gustavo-amaya' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gustavo-amaya' ), 'gustavo-amaya', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ template-parts/components/skills-accordion.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Part
+ * Skills accordion
+ *
+ * @package Gustavo_Amaya
+ * @version 1.0.0
+ */
+?>
+
+<section class="skills-accordion">
+	<h2 class="section-title">Skills</h2>
+
+	<div class="accordion">
+		<button class="accordion__header">Backend<span class="accordion__icon">+</span></button>
+		<div class="accordion__content">
+			<ul>
+				<li>PHP</li>
+				<li>WordPress</li>
+				<li>APIs REST</li>
+				<li>MySQL</li>
+			</ul>
+		</div>
+	</div>
+</section>
