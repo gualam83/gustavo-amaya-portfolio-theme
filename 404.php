bbce381f2a3ef29712579492f8ea4b118301f233
Gustavo Amaya B <gualam@gmail.com>
2026-02-23 10:54:25 -0500

    Styles Fix

--- 404.php
+++ 404.php
@@ -14,42 +14,16 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'gustavo-amaya' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'It looks like nothing was found at this location.', 'gustavo-amaya' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'gustavo-amaya' ); ?></p>
+				<p><?php esc_html_e( 'Maybe try one of the links below or a search?', 'gustavo-amaya' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'gustavo-amaya' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$gustavo_amaya_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'gustavo-amaya' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$gustavo_amaya_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				<div class="error-404-actions">
+					<a href="/" class="btn-primary">Back to Home</a>
+					<a href="/#portfolio" class="btn-secondary">View My Work</a>
+				</div>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
